3430bd58dbb113a0971f52b034cfd0c4f9f4f3bf
Liam Rosenfeld <liamrosenfeld@gmail.com>
2017-10-21 23:40:38 -0400

    Fixed context menu option (sorta)

--- porgify/bg.js
+++ porgify/bg.js
@@ -1,81 +1,82 @@
 ﻿ //porgify background script
 (() => {
 
-    let self = {
-        //Get saved setting and initialize GUI items
-        init() {
-          chrome.storage.sync.get({
-            activate: true,
-            contextmenu: true,
-            contextmenuActivate: true
-          }, function(items) {
-            self.updateContextMenu(items);
-          });
+  let self = {
+    //Get saved setting and initialize GUI items
+    init() {
+      chrome.storage.sync.get({
+        activate: true,
+        contextmenu: true,
+        contextmenuActivate: true
+      }, function(items) {
+        self.updateContextMenu(items);
+      });
 
-          chrome.runtime.onInstalled.addListener(self.onInstalled);
-          chrome.runtime.onMessage.addListener(self.onMessageReceived);
-        },
+      chrome.runtime.onInstalled.addListener(self.onInstalled);
+      chrome.runtime.onMessage.addListener(self.onMessageReceived);
+    },
 
-        //On first install
-        onInstalled(details) {
-          if (details.reason === "install") {
+    //On first install
+    onInstalled(details) {
+      if (details.reason === "install") {
+        self.openOptions();
+      }
+    },
+
+    //On message received
+    onMessageReceived(message, sender, sendResponse) {
+
+      //Option page saved
+      if (message.type === "options") {
+        self.updateContextMenu(message.items);
+      } else if (message.type === "extensions") {
+        self.openExtensions();
+      }
+      if (typeof(sendResponse) === "function")
+        sendResponse();
+    },
+
+    //Update GUI
+    updateContextMenu(items) {
+
+      chrome.contextMenus.remove("porgifyOptions");
+
+      if (items.contextmenu) {
+        chrome.contextMenus.create({
+          "id": "porgifyOptions",
+          "title": chrome.i18n.getMessage("contextMenuOptions"),
+          "contexts": ["page"],
+          "onclick" () {
             self.openOptions();
-          }
-        },
-
-        //On message received
-        onMessageReceived(message, sender, sendResponse) {
-
-          //Option page saved
-          if (message.type === "options") {
-            self.updateContextMenu(message.items);
-          } else if (message.type === "extensions") {
-            self.openExtensions();
-          }
-          if (typeof(sendResponse) === "function")
-            sendResponse();
-        },
-
-        //Update GUI
-        updateContextMenu() {
-
-          chrome.contextMenus.remove("porgifyOptions");
-
-          chrome.contextMenus.create({
-            "id": "porgifyOptions",
-            "title": chrome.i18n.getMessage("contextMenuOptions"),
-            "contexts": ["page"],
-            "onclick" () {
-              self.openOptions();
-            }
-          });
-      },
-
-      //Opens the options tab
-      openOptions() {
-        var optionsUrl = chrome.extension.getURL('porgify/options/options.html');
-        self.openUrl(optionsUrl);
-
-      },
-
-      openUrl(url) {
-        chrome.tabs.query({
-          url
-        }, function(tabs) {
-          if (tabs.length) {
-            chrome.tabs.update(tabs[0].id, {
-              active: true
-            });
-            chrome.windows.update(tabs[0].windowId, {
-              focused: true
-            });
-          } else {
-            chrome.tabs.create({
-              url
-            });
           }
         });
       }
+    },
+
+    //Opens the options tab
+    openOptions() {
+      var optionsUrl = chrome.extension.getURL('porgify/options/options.html');
+      self.openUrl(optionsUrl);
+    },
+
+    openUrl(url) {
+      chrome.tabs.query({
+        url
+      }, function(tabs) {
+        if (tabs.length) {
+          chrome.tabs.update(tabs[0].id, {
+            active: true
+          });
+          chrome.windows.update(tabs[0].windowId, {
+            focused: true
+          });
+        } else {
+          chrome.tabs.create({
+            url
+          });
+        }
+      });
+    }
   };
 
   self.init();
